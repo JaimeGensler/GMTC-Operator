146224edac4bce851a5489f2727f887b98254ad5
thislooksfun <thislooksfun@repbot.org>
2021-01-24 23:05:57 -0600

    Remove now redundant Pseudonym decorator

--- src/BotAPI/Decorators/index.ts
+++ src/BotAPI/Decorators/index.ts
@@ -1,7 +1,5 @@
 import Command from './Command';
 import Conditional from './Conditional';
-import Pseudonym from './Pseudonym';
 
 export { Command };
 export { Conditional };
-export { Pseudonym };
